b1366a39c9c09b448e39d0fe6426040172a17cb9
dawsonrichey <dawson.richey@ttu.edu>
2021-02-14 06:36:11 -0600

    version 4

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(cookieParser());
 
 app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
-    res.render('index');
+    const name = req.cookies.username;
+    if (name) {
+      res.render('index', { name });
+    } else {
+      res.redirect('/hello');
+    }
 });
 
 app.get('/cards', (req, res) => {
@@ -13,7 +23,22 @@ app.get('/cards', (req, res) => {
 });
 
 app.get('/hello', (req, res) => {
-  res.render('hello');
+  const name = req.cookies.username;
+  if (name) {
+    res.redirect('/');
+  } else {
+    res.render('hello');
+  }
+});
+
+app.post('/hello', (req, res) => {
+  res.cookie('username', req.body.username);
+  res.redirect('/');
+});
+
+app.post('/goodbye', (req, res) => {
+  res.clearCookie('username');
+  res.redirect('/hello');
 });
 
 app.listen(3000, () => {
